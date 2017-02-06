9d8882875ffee28e0c63aa3ef46be497b5dbb9ab
Daniel Juncos <daniel.juncos.01@gmail.com>
2017-02-06 17:58:25 -0500

    addition of navbar

---
+++ main.rb
@@ -0,0 +1,20 @@
+require 'sinatra'
+
+
+get '/' do
+	erb :layout
+end
+
+# get '/gallery' do
+# 	erb :gallery
+# end
+
+# get '/contactUs' do
+# 	erb :contactUs
+# end
+
+# get '/home' do
+# 	erb :home
+# end
+
+
